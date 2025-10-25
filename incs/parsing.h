9e8f59f6991432584ee12d6394304c2ff1e4e9f3
PC Portable Corentin <corentin.viel@neuf.fr>
2025-10-25 09:14:57 +0200

    [ADD] added struct for scene data processing functions

--- incs/parsing.h
+++ incs/parsing.h
@@ -6,7 +6,7 @@
 /*   By: cviel <cviel@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/23 17:38:20 by cviel             #+#    #+#             */
-/*   Updated: 2025/10/25 08:55:01 by cviel            ###   ########.fr       */
+/*   Updated: 2025/10/25 09:12:13 by cviel            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,12 @@
 # define WHITE_SPACES "\t\n\v\f\r "
 
 #include "scene.h"
+
+typedef struct s_func
+{
+	char	*name;
+	int		(*f)(char *, t_scene *);
+}	t_func;
 
 int	    parsing(int ac, char **av, t_scene *ptr_scene);
 int	    get_line(int fd, char **ptr_line);
