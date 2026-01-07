c3e72852ec265fd14565977df96b038bf6afd07a
Corentin Viel <cviel@z2r6p1.42lyon.fr>
2026-01-07 22:59:16 +0100

    [MOD/FIX] removed testing printf's

--- srcs/parsing/fill_item.c
+++ srcs/parsing/fill_item.c
@@ -6,7 +6,7 @@
 /*   By: cviel <cviel@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/07 22:29:46 by cviel             #+#    #+#             */
-/*   Updated: 2026/01/07 22:56:11 by cviel            ###   ########.fr       */
+/*   Updated: 2026/01/07 22:58:57 by cviel            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,25 +26,16 @@ int	fill_item(char **line_split, void *ptr_item, const t_parser parser[], size_t
 	void	*curr_field;
 
 	i = 0;
-	printf("parser_size = %zu\n", parser_size);
 	while (i < parser_size)
 	{
-		printf("%zu\n", i);
 		if (line_split[i] == NULL)
-		{
-			printf("out here\n");
 			return (INVALID_FILE);
-		}
 		curr_field = (unsigned char *)ptr_item + parser[i].offset;
 		ret = parser[i].getter(line_split[i], curr_field);
 		if (ret != SUCCESS)
-		{
-			printf("out there\n");
 			return (ret);
-		}
 		++i;
 	}
-	printf("\n");
 	if (line_split[i] != NULL)
 		return (INVALID_FILE);
 	return (SUCCESS);
